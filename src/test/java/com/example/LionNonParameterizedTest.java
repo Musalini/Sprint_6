10d831a826cbe0db4e3342f4221e06c038736d8c
Musa Muradzade <muradzade.musa@gmail.com>
2024-10-18 17:09:27 +0300

    исправил замечания

--- src/test/java/com/example/LionNonParameterizedTest.java
+++ src/test/java/com/example/LionNonParameterizedTest.java
@@ -3,38 +3,20 @@ package com.example;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RunWith(Parameterized.class)
-public class LionTest {
+public class LionNonParameterizedTest {
 
     private Feline feline;
     private Lion lion;
-    private String sex;
-    private boolean hasMane;
-
-    @Parameterized.Parameters
-    public static Object[][] data() {
-        return new Object[][]{
-                {"Самец", true},
-                {"Самка", false}
-        };
-    }
-
-    public LionTest(String sex, boolean hasMane) {
-        this.sex = sex;
-        this.hasMane = hasMane;
-    }
 
     @Before
     public void setUp() throws Exception {
         feline = Mockito.mock(Feline.class);
-        lion = new Lion(feline, sex);
+        lion = new Lion(feline, "Самец");
     }
 
     @Test
@@ -43,13 +25,6 @@ public class LionTest {
         int expectedKittens = 1;
         int actualKittens = lion.getKittens();
         Assert.assertEquals(expectedKittens, actualKittens);
-    }
-
-    @Test
-    public void testVerifyLionHasMane() {
-        boolean expectedHasMane = hasMane;
-        boolean actualHasMane = lion.doesHaveMane();
-        Assert.assertEquals(expectedHasMane, actualHasMane);
     }
 
     @Test
